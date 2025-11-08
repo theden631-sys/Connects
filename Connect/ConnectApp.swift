d2218368f38be6fc16f117353283d99a5a7a5fa7
Christopher Rydberg <Theden631@gmail.com>
2025-11-08 11:06:32 -0500

    Initial Commit

---
+++ Connect/ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectApp.swift
+//  Connect
+//
+//  Created by Christopher Rydberg on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConnectTests/ConnectTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectTests.swift
+//  ConnectTests
+//
+//  Created by Christopher Rydberg on 11/8/25.
+//
+
+import Testing
+@testable import Connect
+
+struct ConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
